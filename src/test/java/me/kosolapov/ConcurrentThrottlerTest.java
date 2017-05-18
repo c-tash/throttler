3a1ccde5b460fa1dd4b1f0c0c5d4974c66b1026c
k.kosolapov <k.kosolapov@samsung.com>
2017-05-18 21:14:01 +0300

    Added test to check ConcurrentThrottler allows all client requests if their total rps is equal to Throttler rps.

--- src/test/java/me/kosolapov/ConcurrentThrottlerTest.java
+++ src/test/java/me/kosolapov/ConcurrentThrottlerTest.java
@@ -38,6 +38,39 @@ public class ConcurrentThrottlerTest {
         checkRps(results, rps);
     }
 
+    @Test
+    public void testRpsWithBusyWait() throws Exception {
+        final int rps = 100000;
+        final int clientCount = 10;
+        final int clientRps = rps / clientCount;
+        final int clientRequests = clientRps*10;
+        final ConcurrentThrottler throttler = new ConcurrentThrottler(rps);
+        final List<ThrottlingResult> results = new ArrayList<>(clientRequests*clientCount);
+        final List<CompletableFuture<List<ThrottlingResult>>> clients = new ArrayList<>(clientCount);
+        final long waitNano = TEST_COUNTER.getSecondAsTime() / clientRps;
+        for (int i = 0; i < clientCount; i++) {
+            clients.add(CompletableFuture.supplyAsync(() -> {
+                final List<ThrottlingResult> throttlingResults = new ArrayList<>(clientRps);
+                for (int j = 0; j < clientRequests; j++) {
+                    final ThrottlingResult throttlingResult = throttler.tryThrottleWithResult(EMPTY_RUNNABLE);
+                    throttlingResults.add(throttlingResult);
+                    final long start = throttlingResult.getTime();
+                    long end;
+                    do {
+                        end = System.nanoTime();
+                    } while (end - start < waitNano);
+                }
+                return throttlingResults;
+            }));
+        }
+        for (final CompletableFuture<List<ThrottlingResult>> client : clients) {
+            results.addAll(client.get());
+        }
+        results.sort(Comparator.comparing(ThrottlingResult::getTime));
+        checkRps(results, rps);
+        assertEquals(clientRequests*clientCount, results.stream().filter(ThrottlingResult::isPassed).count());
+    }
+
     private void checkRps(List<ThrottlingResult> results, long rps) {
         final int requestCount = results.size();
         int leftCursor = 0;
